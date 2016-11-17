a1bc827984a368523a8e36f86ff67eff0808b938
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-17 09:47:16 -0600

    Add conection to firebase db and write

--- app/src/main/java/me/juancrg90/dicistest/MainActivity.java
+++ app/src/main/java/me/juancrg90/dicistest/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -17,15 +20,23 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.button)
     Button button;
 
+    FirebaseDatabase database;
+    DatabaseReference myReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        database = FirebaseDatabase.getInstance();
+        myReference = database.getReference("message");
     }
 
     @OnClick(R.id.button)
     public void clickSubmitHandler() {
-        Log.v("HOLA", "HOLA");
+        String message = editText.getText().toString();
+        myReference.setValue(message);
+        editText.setText("");
     }
 }
